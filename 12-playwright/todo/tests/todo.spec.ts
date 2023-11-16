a5c2b8f569d8aa7add90d5f6d640f2e0ecb37c0f
Volodymyr Kryvoshapov <v.kryvoshapov@gmail.com>
2023-11-16 21:57:17 +0200

    Added mock API example

--- 12-playwright/todo/tests/todo.spec.ts
+++ 12-playwright/todo/tests/todo.spec.ts
@@ -13,11 +13,36 @@ test('verify that "Todo" page has correct title', async ({ page }) => {
 });
 
 test('should display loading spinner', async ({ page }) => {
+  const TODOS = [
+    {
+      "id": 1,
+      "title": "Qui voluptatum.",
+      "done": false,
+      "description": "Officiis ab maxime quisquam ea animi architecto officia ipsum sint similique doloremque nulla corporis doloremque tenetur soluta."
+    },
+    {
+      "id": 2,
+      "title": "Excepturi.",
+      "done": false,
+      "description": "Vero ipsum eaque odio mollitia tempora quos dicta perspiciatis magnam odio."
+    },
+    {
+      "id": 3,
+      "title": "Ut alias.",
+      "done": true,
+      "description": "Molestias rem aliquid aperiam quisquam minima autem ex fugit magnam sed est ipsa totam distinctio optio dolore eos nesciunt laudantium repellendus minus excepturi iste inventore quae corrupti ullam quia."
+    },
+  ]
+
+  await page.route('*/**/todos/', async route => {
+    await route.fulfill({ json: TODOS });
+  });
+
   const progressbar = page.getByRole('progressbar')
 
   await expect(progressbar).not.toBeVisible();
 
   const rows = page.locator('tbody > tr')
 
-  await expect(rows).toHaveCount(10);
+  await expect(rows).toHaveCount(3);
 });
